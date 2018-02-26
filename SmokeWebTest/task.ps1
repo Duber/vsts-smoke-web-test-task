10007c190c7dcba045c8d0149a2637ceb7cd84e8
baileyjs78 <jbailey@northernsafety.com>
2018-02-26 14:03:48 -0500

    Added additional security options

--- SmokeWebTest/task.ps1
+++ SmokeWebTest/task.ps1
@@ -18,6 +18,21 @@ param
 
 Write-Host "Executing web test for $url"
 
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
 $HTTP_Status_Timeout = 0
 $HTTP_Request = [System.Net.WebRequest]::Create($url)
 
